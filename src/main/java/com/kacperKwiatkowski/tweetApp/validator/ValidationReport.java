b255dafbe467c0fc1a67b7b30f61b14d58f1b641
Kacper Kwiatkowski <kacper18k@gmail.com>
2022-09-02 11:30:12 +0100

    Travis config #17

--- src/main/java/com/kacperKwiatkowski/tweetApp/validator/ValidationReport.java
+++ src/main/java/com/kacperKwiatkowski/tweetApp/validator/ValidationReport.java
@@ -1,11 +1,15 @@
 package com.kacperKwiatkowski.tweetApp.validator;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Getter
 public class ValidationReport {
 
