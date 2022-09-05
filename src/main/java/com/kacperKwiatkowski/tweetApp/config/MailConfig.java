387cdc49e4e4eae4f88c641798e9ea2f3cc0820b
Kacper Kwiatkowski <kacper18k@gmail.com>
2022-09-05 16:55:22 +0100

    Travis deployment #5

--- src/main/java/com/kacperKwiatkowski/tweetApp/config/MailConfig.java
+++ src/main/java/com/kacperKwiatkowski/tweetApp/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.kacperKwiatkowski.tweetApp.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
+@Slf4j
 @Configuration
 public class MailConfig {
 
@@ -19,6 +21,9 @@ public class MailConfig {
 
     @Bean
     public JavaMailSender getJavaMailSender() {
+        log.info("IMPORTANT    " + mailUsername);
+        log.info("IMPORTANT    " + mailPassword);
+
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
