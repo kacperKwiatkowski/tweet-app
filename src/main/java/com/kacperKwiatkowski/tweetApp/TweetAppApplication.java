3000ba966e01e34e0a7b89bf1f7ac274a9dff5dc
Kacper Kwiatkowski <kacper18k@gmail.com>
2022-09-24 11:50:20 +0200

    Command line runner commented out

--- src/main/java/com/kacperKwiatkowski/tweetApp/TweetAppApplication.java
+++ src/main/java/com/kacperKwiatkowski/tweetApp/TweetAppApplication.java
@@ -47,130 +47,130 @@ public class TweetAppApplication {
         SpringApplication.run(TweetAppApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            userRepository.deleteAll();
-            tweetRepository.deleteAll();
-
-            userRepository.save(
-                    UserEntity.builder()
-                            .firstName("Adam")
-                            .lastName("Adamski")
-                            .email("adam@gmail.com")
-                            .username("4d4m")
-                            .password(passwordEncoder.encode("password1"))
-                            .avatar(imageService.scaleImage(getBinaryImage("https://img.freepik.com/premium-vector/man-avatar-profile-round-icon_24640-14044.jpg?w=740"), WIDTH, HEIGHT))
-                            .roleType(RoleType.USER.name())
-                            .build()
-            );
-
-
-            userRepository.save(
-                    UserEntity.builder()
-                            .firstName("Maria")
-                            .lastName("Kowalska")
-                            .email("maria@gmail.com")
-                            .username("m4ria")
-                            .password(passwordEncoder.encode("password2"))
-                            .avatar(imageService.scaleImage(getBinaryImage("https://img.freepik.com/premium-vector/woman-avatar-profile-round-icon_24640-14047.jpg?w=740"), WIDTH, HEIGHT))
-                            .roleType(RoleType.USER.name())
-                            .build()
-            );
-
-            userRepository.save(
-                    UserEntity.builder()
-                            .firstName("Bożena")
-                            .lastName("Czartoryska")
-                            .email("bozena@gmail.com")
-                            .username("b0zen4")
-                            .password(passwordEncoder.encode("password3"))
-                            .avatar(imageService.scaleImage(getBinaryImage("https://img.freepik.com/premium-vector/woman-avatar-profile-round-icon_24640-14042.jpg?w=740"), WIDTH, HEIGHT))
-                            .roleType(RoleType.USER.name())
-                            .build()
-            );
-
-            UUID thread1 = UUID.randomUUID();
-
-            userRepository.findAll().forEach(
-                    user -> {
-                        tweetRepository.save(
-                                TweetEntity.builder()
-                                        .title("TITLE_" + staticTitleCount++)
-                                        .message("MESSAGE_" + staticMessageCount++)
-                                        .postDateTime(LocalDateTime.parse(LocalDateTime.now().minusDays(staticDaysMinusCount++).toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
-                                        .username("4d4m")
-                                        .threadId(UUID.randomUUID())
-                                        .build()
-                        );
-
-                        tweetRepository.save(
-                                TweetEntity.builder()
-                                        .title("TITLE_" + staticTitleCount++)
-                                        .message("MESSAGE_" + staticMessageCount++)
-                                        .postDateTime(LocalDateTime.parse(LocalDateTime.now().minusDays(staticDaysMinusCount++).toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
-                                        .username("m4ria")
-                                        .threadId(thread1)
-                                        .build()
-                        );
-
-                        tweetRepository.save(
-                                TweetEntity.builder()
-                                        .title("TITLE_" + staticTitleCount++)
-                                        .message("MESSAGE_" + staticMessageCount++)
-                                        .postDateTime(LocalDateTime.parse(LocalDateTime.now().minusDays(staticDaysMinusCount++).toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
-                                        .username("b0zen4")
-                                        .threadId(thread1)
-                                        .build()
-                        );
-                    }
-            );
-
-            tweetRepository.save(
-                    TweetEntity.builder()
-                            .title("TITLE_" + staticTitleCount++)
-                            .message("MESSAGE_" + staticMessageCount++)
-
-
-                            .postDateTime(LocalDateTime.parse(LocalDateTime.now().toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
-                            .username("4d4m")
-                            .threadId(thread1)
-                            .build()
-            );
-
-            tweetRepository.save(
-                    TweetEntity.builder()
-                            .title("TITLE_" + staticTitleCount++)
-                            .message("MESSAGE_" + staticMessageCount++)
-                            .postDateTime(LocalDateTime.parse(LocalDateTime.now().toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
-                            .username("4d4m")
-                            .threadId(thread1)
-                            .build()
-            );
-
-            tweetRepository.save(
-                    TweetEntity.builder()
-                            .title("TITLE_" + staticTitleCount++)
-                            .message("SHOULD BE FIRST")
-                            .postDateTime(LocalDateTime.parse(LocalDateTime.now().toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
-                            .username("m4ria")
-                            .threadId(UUID.randomUUID())
-                            .build()
-            );
-
-            userRepository.findAll().forEach(user -> log.info(user.toString()));
-            tweetRepository.findAll().forEach(tweet -> log.info(tweet.toString()));
-        };
-    }
-
-    private byte[] getBinaryImage(String path) {
-        try {
-            URL url = new URL(path);
-            InputStream is = url.openStream();
-            return is.readAllBytes();
-        } catch (IOException e) {
-            throw new RuntimeException();
-        }
-    }
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//
+//            userRepository.deleteAll();
+//            tweetRepository.deleteAll();
+//
+//            userRepository.save(
+//                    UserEntity.builder()
+//                            .firstName("Adam")
+//                            .lastName("Adamski")
+//                            .email("adam@gmail.com")
+//                            .username("4d4m")
+//                            .password(passwordEncoder.encode("password1"))
+//                            .avatar(imageService.scaleImage(getBinaryImage("https://img.freepik.com/premium-vector/man-avatar-profile-round-icon_24640-14044.jpg?w=740"), WIDTH, HEIGHT))
+//                            .roleType(RoleType.USER.name())
+//                            .build()
+//            );
+//
+//
+//            userRepository.save(
+//                    UserEntity.builder()
+//                            .firstName("Maria")
+//                            .lastName("Kowalska")
+//                            .email("maria@gmail.com")
+//                            .username("m4ria")
+//                            .password(passwordEncoder.encode("password2"))
+//                            .avatar(imageService.scaleImage(getBinaryImage("https://img.freepik.com/premium-vector/woman-avatar-profile-round-icon_24640-14047.jpg?w=740"), WIDTH, HEIGHT))
+//                            .roleType(RoleType.USER.name())
+//                            .build()
+//            );
+//
+//            userRepository.save(
+//                    UserEntity.builder()
+//                            .firstName("Bożena")
+//                            .lastName("Czartoryska")
+//                            .email("bozena@gmail.com")
+//                            .username("b0zen4")
+//                            .password(passwordEncoder.encode("password3"))
+//                            .avatar(imageService.scaleImage(getBinaryImage("https://img.freepik.com/premium-vector/woman-avatar-profile-round-icon_24640-14042.jpg?w=740"), WIDTH, HEIGHT))
+//                            .roleType(RoleType.USER.name())
+//                            .build()
+//            );
+//
+//            UUID thread1 = UUID.randomUUID();
+//
+//            userRepository.findAll().forEach(
+//                    user -> {
+//                        tweetRepository.save(
+//                                TweetEntity.builder()
+//                                        .title("TITLE_" + staticTitleCount++)
+//                                        .message("MESSAGE_" + staticMessageCount++)
+//                                        .postDateTime(LocalDateTime.parse(LocalDateTime.now().minusDays(staticDaysMinusCount++).toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
+//                                        .username("4d4m")
+//                                        .threadId(UUID.randomUUID())
+//                                        .build()
+//                        );
+//
+//                        tweetRepository.save(
+//                                TweetEntity.builder()
+//                                        .title("TITLE_" + staticTitleCount++)
+//                                        .message("MESSAGE_" + staticMessageCount++)
+//                                        .postDateTime(LocalDateTime.parse(LocalDateTime.now().minusDays(staticDaysMinusCount++).toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
+//                                        .username("m4ria")
+//                                        .threadId(thread1)
+//                                        .build()
+//                        );
+//
+//                        tweetRepository.save(
+//                                TweetEntity.builder()
+//                                        .title("TITLE_" + staticTitleCount++)
+//                                        .message("MESSAGE_" + staticMessageCount++)
+//                                        .postDateTime(LocalDateTime.parse(LocalDateTime.now().minusDays(staticDaysMinusCount++).toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
+//                                        .username("b0zen4")
+//                                        .threadId(thread1)
+//                                        .build()
+//                        );
+//                    }
+//            );
+//
+//            tweetRepository.save(
+//                    TweetEntity.builder()
+//                            .title("TITLE_" + staticTitleCount++)
+//                            .message("MESSAGE_" + staticMessageCount++)
+//
+//
+//                            .postDateTime(LocalDateTime.parse(LocalDateTime.now().toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
+//                            .username("4d4m")
+//                            .threadId(thread1)
+//                            .build()
+//            );
+//
+//            tweetRepository.save(
+//                    TweetEntity.builder()
+//                            .title("TITLE_" + staticTitleCount++)
+//                            .message("MESSAGE_" + staticMessageCount++)
+//                            .postDateTime(LocalDateTime.parse(LocalDateTime.now().toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
+//                            .username("4d4m")
+//                            .threadId(thread1)
+//                            .build()
+//            );
+//
+//            tweetRepository.save(
+//                    TweetEntity.builder()
+//                            .title("TITLE_" + staticTitleCount++)
+//                            .message("SHOULD BE FIRST")
+//                            .postDateTime(LocalDateTime.parse(LocalDateTime.now().toString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME).format(DateTimeFormatter.ofPattern(PATTERN)))
+//                            .username("m4ria")
+//                            .threadId(UUID.randomUUID())
+//                            .build()
+//            );
+//
+//            userRepository.findAll().forEach(user -> log.info(user.toString()));
+//            tweetRepository.findAll().forEach(tweet -> log.info(tweet.toString()));
+//        };
+//    }
+//
+//    private byte[] getBinaryImage(String path) {
+//        try {
+//            URL url = new URL(path);
+//            InputStream is = url.openStream();
+//            return is.readAllBytes();
+//        } catch (IOException e) {
+//            throw new RuntimeException();
+//        }
+//    }
 }
